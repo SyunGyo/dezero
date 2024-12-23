9cbc146190b38bb674e5856163f244793149bd62
Takeuchi Toshiaki <syungyo0120@gmail.com>
2024-12-23 18:50:31 +0900

    layerを追加

--- dezero/core.py
+++ dezero/core.py
@@ -116,6 +116,37 @@ class Variable:
         p = str(self.data).replace('\n', '\n' + ' ' * 9)
         return 'variable(' + p + ')'
     
+class Parameter(Variable):
+    pass
+
+class Layer:
+    def __init__(self):
+        self._params = set()
+
+    def __setattr__(self, name, value):
+        if isinstance(value, Parameter):
+            self._params.add(name)
+        super().__setattr__(name,value)
+
+    def __call__(self, *inputs):
+        outputs = self.forward(*inputs)
+        if not isinstance(outputs, tuple):
+            outputs = (outputs,)
+        self.inputs = [weakref.ref(x) for x in inputs]
+        self.outpus = [weakref.ref(y) for y in outputs] 
+
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, inputs):
+        raise NotImplementedError
+    
+    def params(self):
+        for name in self._params:
+            yield self.__dict__[name]
+
+    def cleargrads(self):
+        for param in self.params():
+            param.cleargrad()
 
 
 
